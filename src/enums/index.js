36ee9d497f71ccff9b2640980b440ea062ddebfe
divinirakiza <irakizadivin@gmail.com>
2022-05-16 17:20:48 +0200

    feat: add common utils

---
+++ src/enums/index.js
@@ -0,0 +1,14 @@
+const EUserType = {
+    ADMIN: 'ADMIN',
+    AUTHOR: 'AUTHOR',
+    NORMAL: 'NORMAL',
+};
+
+const EUserStatus = {
+    PENDING: 'PENDING',
+    ACTIVE: 'ACTIVE',
+    DELETED: 'DELETED'
+};
+
+
+module.exports = {EUserType, EUserStatus};
