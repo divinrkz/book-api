4a36adf69ab718d2a14db656e22a508548f9a07f
divinirakiza <irakizadivin@gmail.com>
2022-05-17 17:49:08 +0200

    feat: add config

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,49 +1,46 @@
 const {User, validate, validateUpdate} = require('../models/user.model');
-const { ERROR_RESPONSE, SUCCESS_RESPONSE, hashPassword } = require('../utils/common.util');
+const { hashPassword } = require('../utils/common.util');
 const {EUserType} = require('../enums');
+const { APIResponse } = require('../config/APIResponse.config');
 
 
 const getAll = async (req, res) => {
     try {
         const users = await User.find();
-        return res.status(200).send(SUCCESS_RESPONSE(users));
+        return res.status(200).send(APIResponse.success(users));
     } catch (err) {
-        return res.status(500).send(ERROR_RESPONSE(err.toString()));
+        return res.status(500).send(APIResponse.fail(err.toString()));
     } 
 };
 
 const getById = async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).send(ERROR_RESPONSE(null, 'NOT FOUND')); 
+        if (!user) return res.status(404).send(APIResponse.fail(null, 'NOT FOUND')); 
 
-        return res.status(200).send(SUCCESS_RESPONSE(user));
+        return res.status(200).send(APIResponse.success(user));
     } catch (err) {
-        return res.status(500).send(ERROR_RESPONSE(err.toString()));
+        return res.status(500).send(APIResponse.fail(err.toString()));
     } 
 };
-
-
 
 
 const create = async (req, res) => {
     try {
         const {error} = validate(req.body);
-        if (error) return res.status(400).send(ERROR_RESPONSE(error.details[0].message, 'VALIDATION ERROR')); 
+        if (error) return res.status(400).send(APIResponse.fail(error.details[0].message, 'VALIDATION ERROR')); 
 
         req.body.password = await hashPassword(req.body.password);
-       
-        req.body.status = EUserStatus.ACTIVE;
         req.body.userType = EUserType.ADMIN;
 
         const user = new User(req.body);
 
         const saved = await user.save();
 
-        return res.status(200).send(SUCCESS_RESPONSE(saved));
+        return res.status(200).send(APIResponse.success(saved));
     } catch (err) {
         console.error(err);
-        return res.status(500).send(ERROR_RESPONSE(err.toString()));
+        return res.status(500).send(APIResponse.fail(err.toString()));
     } 
 };
 
@@ -51,11 +48,11 @@ const create = async (req, res) => {
 const update = async (req, res) => {
     try {
         const {error} = validateUpdate(req.body);
-        if (error) return res.status(400).send(ERROR_RESPONSE(error.details[0].message, 'VALIDATION ERROR')); 
+        if (error) return res.status(400).send(APIResponse.fail(error.details[0].message, 'VALIDATION ERROR')); 
 
 
         const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).send(ERROR_RESPONSE(null, 'NOT FOUND'));  
+        if (!user) return res.status(404).send(APIResponse.fail(null, 'NOT FOUND'));  
 
         user.names = req.body.names;
         user.username = req.body.username;
@@ -63,9 +60,9 @@ const update = async (req, res) => {
 
         const updated = await user.save();
 
-        return res.status(200).send(SUCCESS_RESPONSE(updated));
+        return res.status(200).send(APIResponse.success(updated));
     } catch (err) {
-        return res.status(500).send(ERROR_RESPONSE(err.toString()));
+        return res.status(500).send(APIResponse.fail(err.toString()));
     } 
 };
 
@@ -73,13 +70,13 @@ const update = async (req, res) => {
 const deleter = async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).send(ERROR_RESPONSE(null, 'NOT FOUND'));  
+        if (!user) return res.status(404).send(APIResponse.fail(null, 'NOT FOUND'));  
 
         const deleted = await User.findByIdAndDelete(user._id);
 
         return res.status(200).send(SUCCESS_RESPONSE(null, 'Deleted Successfully'));
     } catch (err) {
-        return res.status(500).send(ERROR_RESPONSE(err.toString()));
+        return res.status(500).send(APIResponse.fail(err.toString()));
     } 
 };
 
